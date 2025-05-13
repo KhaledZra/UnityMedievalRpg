aa969ec1e0a002b470d0223cc76b44046987dbc2
halo_ <halo_434000@hotmail.com>
2025-05-13 13:32:34 +0200

    feat/fix: got jumping and gravity to a working state although not happy with it. Added new support for sprint animations which is also a WIP.

--- Assets/Player/Scripts/PlayerAnimationHandler.cs
+++ Assets/Player/Scripts/PlayerAnimationHandler.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 
 public class PlayerAnimationHandler : MonoBehaviour
 {
-    [Header("Animation")]
-    [SerializeField] private Animator _animator;
+    [Header("Animation")] [SerializeField] private Animator _animator;
     [SerializeField] private float _animationBlendSpeed = 0.02f;
-    
+    [SerializeField] private PlayerMovementState _playerMovementState;
+
     // So we don't have to use strings
     private static readonly int InputXHash = Animator.StringToHash("InputX");
     private static readonly int InputYHash = Animator.StringToHash("InputY");
-    
+    private static readonly int InputMagHash = Animator.StringToHash("InputMagnitude");
+
     private Vector2 _currentBlendInput = Vector2.zero;
 
     private void Update()
@@ -20,13 +21,21 @@ public class PlayerAnimationHandler : MonoBehaviour
 
     private void UpdateAnimationState()
     {
-        Vector2 inputTarget = PlayerInputHandler.Instance.MovementInputValue;
+        bool isSprinting = _playerMovementState.CurrentMovementState ==
+                           PlayerMovementState.EPlayerMovementState.Sprinting;
         
+        Vector2 inputTarget = isSprinting
+            ? PlayerInputHandler.Instance.MovementInputValue * 1.5f
+            : PlayerInputHandler.Instance.MovementInputValue;
+
+
         // Lerping the input values for smoother transitions
         _currentBlendInput = Vector2.Lerp(_currentBlendInput, inputTarget, _animationBlendSpeed * Time.deltaTime);
-        
+
         // Set the animator parameters
         _animator.SetFloat(InputXHash, _currentBlendInput.x);
         _animator.SetFloat(InputYHash, _currentBlendInput.y);
+
+        _animator.SetFloat(InputMagHash, _currentBlendInput.magnitude);
     }
 }
--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -1,31 +1,30 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 // TODO: handle delayed input actions
 // TODO: Refactor player state variables into a single state object "StateMachine"
 // TODO: camera sway
 
-// TODO: Jump and Gravity speed are a little broken RN. FIX THIS!!!!!!!!!!!!!!!!!!!
-
 public class PlayerMovement : MonoBehaviour
 {
     // Data Objects
-    [Header("Data Objects")]
-    [SerializeField]
+    [Header("Data Objects")] [SerializeField]
     private MovementValues _movementValues;
-    
+
     // Component references
-    [Header("Components")]
-    [SerializeField]
+    [Header("Components")] [SerializeField]
     private CharacterController _characterController;
-    
-    [SerializeField]
-    private Transform _cameraTransform;
-    
+
+    [SerializeField] private Transform _cameraTransform;
+
     // Player state variables
-    [Header("Player State")] 
-    public PlayerMovementState _playerMovementState;
-    
+    [Header("Player State")] public PlayerMovementState _playerMovementState;
+
+    // todo: move this to the PlayerMovementState class
+    private bool _wantsToSprint;
+    private bool _wantsToJump;
+
     private float CurrentMoveSpeed
     {
         get
@@ -34,15 +33,15 @@ public class PlayerMovement : MonoBehaviour
             // if (_isCrouching) return _movementValues.crouchSpeed; 
             if (_playerMovementState.CurrentMovementState == PlayerMovementState.EPlayerMovementState.Sprinting)
                 return _movementValues.sprintSpeed;
-            
+
             return _movementValues.walkSpeed;
         }
     }
-    
+
     // for debugging
     [Header("Debugging")] [field: SerializeField]
     public Vector3 Velocity;
-    
+
     private void OnEnable()
     {
         // Bind the input actions
@@ -50,7 +49,7 @@ public class PlayerMovement : MonoBehaviour
         PlayerInputHandler.Instance.SprintInputAction += OnSprintInput;
         // PlayerInputHandler.Instance.CrouchInputAction += OnCrouchInput;
     }
-    
+
     private void OnDisable()
     {
         // Unbind the input actions
@@ -65,75 +64,102 @@ public class PlayerMovement : MonoBehaviour
         MovementUpdate();
     }
 
-    
+
     // todo: move this to the PlayerMovementState class
     // todo: might switch to velocity based system in the future instead of input based
     private void UpdateMovementState()
     {
-        // Player is not moving
-        if (Vector2.Equals(PlayerInputHandler.Instance.MovementInputValue, Vector2.zero))
+        // Jump/Falling state check is based on the character controller velocity
+        // todo: needs refactoring, it's pretty sensitive to the velocity so if we are moving quickly downhill it
+        // todo: could be seen as falling or running up a hill quickly could be jumping!
+        // todo: i think the fix is a better isGrounded check since current seems flaky
+        if (!_characterController.isGrounded && !IsBetween(_characterController.velocity.y, -2f, 2f)) // Not grounded
         {
-            // Set the movement state to idle
+            if (_characterController.velocity.y < 0f) // Falling
+            {
+                _playerMovementState.SetMovementState(PlayerMovementState.EPlayerMovementState.Falling);
+            }
+            else if (_characterController.velocity.y > 0f) // Jumping
+            {
+                _playerMovementState.SetMovementState(PlayerMovementState.EPlayerMovementState.Jumping);
+            }
+        }
+        // Player is not moving
+        else if (Vector2.Equals(PlayerInputHandler.Instance.MovementInputValue, Vector2.zero))
+        {
+            // Idle
             _playerMovementState.SetMovementState(PlayerMovementState.EPlayerMovementState.Idle);
         }
         // Player is moving
         else
         {
-            // Set the movement state to walking
-            _playerMovementState.SetMovementState(PlayerMovementState.EPlayerMovementState.Walking);
+            // Check if the player is sprinting
+            if (_wantsToSprint)
+            {
+                // Sprinting
+                _playerMovementState.SetMovementState(PlayerMovementState.EPlayerMovementState.Sprinting);
+            }
+            else
+            {
+                // Walking
+                _playerMovementState.SetMovementState(PlayerMovementState.EPlayerMovementState.Walking);
+            }
         }
     }
 
     private void MovementUpdate()
     {
-        Vector3 cameraForwardXZ = new Vector3(_cameraTransform.transform.forward.x, 0f, _cameraTransform.transform.forward.z).normalized;
-        Vector3 cameraRightXZ = new Vector3(_cameraTransform.transform.right.x, 0f, _cameraTransform.transform.right.z).normalized;
-        Vector3 movementDirection = cameraRightXZ * 
-            PlayerInputHandler.Instance.MovementInputValue.x + cameraForwardXZ * PlayerInputHandler.Instance.MovementInputValue.y;
+        Vector3 cameraForwardXZ =
+            new Vector3(_cameraTransform.transform.forward.x, 0f, _cameraTransform.transform.forward.z).normalized;
+        Vector3 cameraRightXZ = new Vector3(_cameraTransform.transform.right.x, 0f, _cameraTransform.transform.right.z)
+            .normalized;
+        Vector3 movementDirection = cameraRightXZ *
+                                    PlayerInputHandler.Instance.MovementInputValue.x +
+                                    cameraForwardXZ * PlayerInputHandler.Instance.MovementInputValue.y;
 
         Vector3 movementDelta = movementDirection * _movementValues.accelerationSpeed * Time.deltaTime;
         Vector3 newVelocity = _characterController.velocity + movementDelta;
 
         // Add drag to player
         Vector3 currentDrag = newVelocity.normalized * _movementValues.dragSpeed * Time.deltaTime;
-        newVelocity = (newVelocity.magnitude > _movementValues.dragSpeed * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
+        newVelocity = (newVelocity.magnitude > _movementValues.dragSpeed * Time.deltaTime)
+            ? newVelocity - currentDrag
+            : Vector3.zero;
         newVelocity = Vector3.ClampMagnitude(newVelocity, CurrentMoveSpeed);
-        
-        // Apply Jump if state is active
-        newVelocity.y = ApplyJump();
-        
+
         // Apply gravity
-        newVelocity.y += _movementValues.gravityValue * Time.deltaTime;
+        newVelocity.y = _characterController.velocity.y + _movementValues.gravityValue * Time.deltaTime;
+
+        // Apply Jump if state is active
+        newVelocity.y += ApplyJump();
 
         // Move character (Unity suggests only calling this once per tick)
         _characterController.Move(newVelocity * Time.deltaTime);
-        
+
         Velocity = newVelocity;
     }
-    
+
     private float ApplyJump()
     {
-        if (_playerMovementState.CurrentMovementState == PlayerMovementState.EPlayerMovementState.Jumping)
+        if (_wantsToJump)
         {
-            // todo: probably needs a check to see if the player is grounded or w/e
-            _playerMovementState.SetMovementState(PlayerMovementState.EPlayerMovementState.Idle);
-            return Mathf.Sqrt(_movementValues.jumpForce * -2.0f * _movementValues.gravityValue);
+            _wantsToJump = false;
+            return Mathf.Sqrt(_movementValues.jumpForce * -3.0f * _movementValues.gravityValue);
         }
 
         return 0f;
     }
-    
+
     private void OnJumpInput()
     {
         // Handle jump input
         if (_characterController.isGrounded)
         {
             // Set the jump flag to true
-            _playerMovementState.SetMovementState(PlayerMovementState.EPlayerMovementState.Jumping);
-
+            _wantsToJump = true;
         }
     }
-    
+
     // todo: might remove crouching completly
     // private void OnCrouchInput(bool wantsToCrouch)
     // {
@@ -164,15 +190,19 @@ public class PlayerMovement : MonoBehaviour
         if (wantsToSprint && _characterController.isGrounded)
         {
             // Set the sprint flag to true and increase the move speed
-            _playerMovementState.SetMovementState(PlayerMovementState.EPlayerMovementState.Sprinting);
+            _wantsToSprint = true;
         }
-        else if (!wantsToSprint && _playerMovementState.CurrentMovementState == PlayerMovementState.EPlayerMovementState.Sprinting)
+        else if (!wantsToSprint && _playerMovementState.CurrentMovementState ==
+                 PlayerMovementState.EPlayerMovementState.Sprinting)
         {
             // Reset the sprint flag to false and decrease the move speed
-            
-            // todo: needs to be set back to appropriate state
-            _playerMovementState.SetMovementState(PlayerMovementState.EPlayerMovementState.Idle);
-            
+            _wantsToSprint = false;
         }
+    }
+
+    // todo: move to new static math class
+    public bool IsBetween(double testValue, double bound1, double bound2)
+    {
+        return (testValue >= Math.Min(bound1, bound2) && testValue <= Math.Max(bound1, bound2));
     }
 }
