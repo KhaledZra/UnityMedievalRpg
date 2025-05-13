44945e80c19648a3c5b7f61c4f3110bab7241af7
halo_ <halo_434000@hotmail.com>
2025-05-13 16:17:07 +0200

    chore: removed unused code since it's saved in vc.

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Linq;
 using UnityEngine;
 
 // TODO: handle delayed input actions
-// TODO: Refactor player state variables into a single state object "StateMachine"
-// TODO: camera sway
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -30,7 +26,6 @@ public class PlayerMovement : MonoBehaviour
         get
         {
             // Determine the current move speed based on player state
-            // if (_isCrouching) return _movementValues.crouchSpeed; 
             if (_playerMovementState.CurrentMovementState == PlayerMovementState.EPlayerMovementState.Sprinting)
                 return _movementValues.sprintSpeed;
 
@@ -47,7 +42,6 @@ public class PlayerMovement : MonoBehaviour
         // Bind the input actions
         PlayerInputHandler.Instance.JumpInputAction += OnJumpInput;
         PlayerInputHandler.Instance.SprintInputAction += OnSprintInput;
-        // PlayerInputHandler.Instance.CrouchInputAction += OnCrouchInput;
     }
 
     private void OnDisable()
@@ -55,7 +49,6 @@ public class PlayerMovement : MonoBehaviour
         // Unbind the input actions
         PlayerInputHandler.Instance.JumpInputAction -= OnJumpInput;
         PlayerInputHandler.Instance.SprintInputAction -= OnSprintInput;
-        // PlayerInputHandler.Instance.CrouchInputAction -= OnCrouchInput;
     }
 
     private void FixedUpdate()
@@ -160,30 +153,6 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    // todo: might remove crouching completly
-    // private void OnCrouchInput(bool wantsToCrouch)
-    // {
-    //     // handle crouch action
-    //     if (wantsToCrouch && !_isSprinting)
-    //     {
-    //         // Set the crouch flag to true and decrease the move speed
-    //         _isCrouching = true;
-    //         
-    //         // shrink the player collider
-    //         _characterController.height = 0.75f;
-    //         transform.localScale = new Vector3(1f, 0.5f, 1f);
-    //     }
-    //     else if (!wantsToCrouch && _isCrouching)
-    //     {
-    //         // Reset the crouch flag to false and increase the move speed
-    //         _isCrouching = false;
-    //         
-    //         // shrink the player collider
-    //         _characterController.height = 2f; // Set the height of the character controller to crouch height
-    //         transform.localScale = Vector3.one; // Set the scale of the player object to normal height
-    //     }
-    // }
-
     private void OnSprintInput(bool wantsToSprint)
     {
         // handle sprint action
@@ -198,11 +167,5 @@ public class PlayerMovement : MonoBehaviour
             // Reset the sprint flag to false and decrease the move speed
             _wantsToSprint = false;
         }
-    }
-
-    // todo: move to new static math class
-    public bool IsBetween(double testValue, double bound1, double bound2)
-    {
-        return (testValue >= Math.Min(bound1, bound2) && testValue <= Math.Max(bound1, bound2));
     }
 }
